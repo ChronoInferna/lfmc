ac5c5bafa27e69d59eb82fd2174899a7282ec15d
Oliver Deng <oliver.t.deng@gmail.com>
2025-12-18 17:10:02 -0500

    feat(ScopedTimer): raii timer

--- include/lfmc/timer.hpp
+++ include/lfmc/timer.hpp
@@ -5,11 +5,13 @@
 namespace lfmc {
 
 class Timer {
+    using clock = std::chrono::high_resolution_clock;
+
   private:
-    std::chrono::high_resolution_clock::time_point start_time_;
+    clock::time_point start_time_;
 
   public:
-    Timer() : start_time_(std::chrono::high_resolution_clock::now()) {}
+    Timer() : start_time_(clock::now()) {}
     ~Timer() = default;
 
     void reset();
@@ -17,15 +19,16 @@ class Timer {
 };
 
 // RAII timer that measures the time duration of a scope
-// TODO unsure of how we want to implement this
 class ScopedTimer {
+    using clock = std::chrono::high_resolution_clock;
+
   private:
-    std::chrono::high_resolution_clock::time_point start_time_;
+    long long* out_;
+    clock::time_point start_time_;
 
   public:
-    ScopedTimer() : start_time_(std::chrono::high_resolution_clock::now()) {}
-    // TODO use destructor? Then how to return the time?
-    ~ScopedTimer();
+    explicit ScopedTimer(long long& out) noexcept : out_(&out), start_time_(clock::now()) {}
+    ~ScopedTimer() noexcept;
 };
 
 } // namespace lfmc
--- src/timer.cpp
+++ src/timer.cpp
@@ -3,20 +3,18 @@
 namespace lfmc {
 
 void Timer::reset() {
-    start_time_ = std::chrono::high_resolution_clock::now();
+    start_time_ = clock::now();
 }
 
 long long Timer::elapsedMilliseconds() const {
-    auto current_time = std::chrono::high_resolution_clock::now();
     auto duration =
-        std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time_).count();
+        std::chrono::duration_cast<std::chrono::milliseconds>(clock::now() - start_time_).count();
     return duration;
 }
 
-ScopedTimer::~ScopedTimer() {
-    // auto end_time = std::chrono::high_resolution_clock::now();
-    // auto duration =
-    //     std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time_).count();
+ScopedTimer::~ScopedTimer() noexcept {
+    *out_ =
+        std::chrono::duration_cast<std::chrono::milliseconds>(clock::now() - start_time_).count();
 }
 
 } // namespace lfmc
