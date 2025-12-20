e8cb7892123ecba2b6d39d0084f8f8b13613bd16
Oliver Deng <oliver.t.deng@gmail.com>
2025-12-20 15:45:09 -0500

    feat(manager.hpp): create framework for stochastic processes and numerical schemes

---
+++ include/lfmc/manager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "lfmc/process.hpp"
+#include "lfmc/scheme.hpp"
+
+/**
+ * @file manager.hpp
+ * @brief Manager class for handling processes and schemes using the Strategy Pattern.
+ *
+ * This file defines the `Manager` class, which is responsible for managing
+ * multiple processes and schemes within the LFMC framework using the Strategy Pattern.
+ *
+ * @see Process
+ * @see Scheme
+ */
+
+namespace lfmc {
+
+/**
+ * @class Manager
+ * @brief Manages processes and schemes using the Strategy Pattern.
+ *
+ * The `Manager` class is responsible for handling multiple processes and schemes,
+ * allowing for dynamic selection and management of different strategies at runtime.
+ */
+class Manager {
+  public:
+    /// Constructor
+    Manager() = default;
+    // Additional methods for managing processes and schemes will be added here
+
+  private:
+    // Internal data structures for managing processes and schemes will be added here
+    // TODO decide on the appropriate strategy implementation - refer to notes and design patterns
+    // TODO figure out how processes and schemes will interact with each other and decide
+    // appropriate pattern
+};
+
+} // namespace lfmc
---
+++ include/lfmc/scheme.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+/* @file scheme.hpp
+ * @brief Scheme class representing numerical schemes for stochastic processes.
+ *
+ * This file defines the `Scheme` class and implementations of different numerical schemes
+ * used for simulating stochastic processes.
+ */
