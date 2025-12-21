ba1dcda5f2029fb9cd94460ab01dfe59f1bf07c1
Oliver Deng <oliver.t.deng@gmail.com>
2025-12-20 19:38:42 -0500

    refactor: flesh out the architecture a bit more

--- include/lfmc/scheme.hpp
+++ include/lfmc/scheme.hpp
@@ -6,3 +6,13 @@
  * This file defines the `Scheme` class and implementations of different numerical schemes
  * used for simulating stochastic processes.
  */
+
+namespace lfmc {
+
+class Scheme {
+  public:
+    virtual ~Scheme() = default;
+    // Define common interface methods for schemes here
+};
+
+} // namespace lfmc
