9b412936a72e87fbcd41d4a96774e9779ea4ff16
Oliver Deng <oliver.t.deng@gmail.com>
2026-01-05 23:59:31 -0500

    test(test_manager.hpp): new tests for managers

---
+++ tests/test_manager.cpp
@@ -0,0 +1,25 @@
+#include "lfmc/Manager.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <memory>
+
+TEST_CASE("Manager initializes", "[Manager]") {
+    SECTION("Initialization with temporaries") {
+        REQUIRE_NOTHROW(lfmc::Manager<lfmc::GeometricBrownianMotion,
+                                      lfmc::EulerMaruyama<lfmc::GeometricBrownianMotion>>(
+            lfmc::GeometricBrownianMotion{.mu = 0.05, .sigma = 0.2},
+            lfmc::EulerMaruyama<lfmc::GeometricBrownianMotion>(),
+            std::make_unique<lfmc::NoVarianceReduction>()));
+    };
+
+    SECTION("Initialization with named objects") {
+        lfmc::GeometricBrownianMotion gbm{.mu = 0.05, .sigma = 0.2};
+        lfmc::EulerMaruyama<lfmc::GeometricBrownianMotion> scheme;
+        std::unique_ptr<lfmc::VarianceReductionStrategy> vrStrategy =
+            std::make_unique<lfmc::NoVarianceReduction>();
+
+        REQUIRE_NOTHROW(lfmc::Manager<lfmc::GeometricBrownianMotion,
+                                      lfmc::EulerMaruyama<lfmc::GeometricBrownianMotion>>(
+            gbm, scheme, std::move(vrStrategy)));
+    };
+}
--- tests/test_timing.cpp
+++ tests/test_timing.cpp
@@ -1,4 +1,4 @@
-#include "lfmc/timer.hpp"
+#include "lfmc/timing.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <thread>
