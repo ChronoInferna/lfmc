3e11fe87650cad15473eab628f95001f23cc22b1
Oliver Deng <oliver.t.deng@gmail.com>
2026-01-14 15:29:06 -0500

    feat: more constraints on concepts for clarity and compile-time sizing of number of testing threads

--- include/lfmc/NumericalScheme.hpp
+++ include/lfmc/NumericalScheme.hpp
@@ -26,9 +26,10 @@ namespace lfmc {
  * @param dt The time step size.
  * @param dW The Wiener increment.
  */
-template <class S, class P>
+template <typename S>
 concept NumericalScheme =
-    StochasticProcess<P> && requires(S const& s, P const& p, double x, double dt, double dW) {
+    requires { typename S::process_type; } && StochasticProcess<typename S::process_type> &&
+    requires(S const& s, typename S::process_type const& p, double x, double dt, double dW) {
         { s.step(p, x, dt, dW) } -> std::same_as<double>;
     };
 
@@ -42,6 +43,9 @@ concept NumericalScheme =
  * @tparam P The stochastic process type used in the SDE.
  */
 template <StochasticProcess P> struct EulerMaruyama {
+    // Expose the process type for compatibility with the concept
+    using process_type = P;
+
     /**
      * @brief Compute the next state using the Euler-Maruyama method.
      * @param process The stochastic process defining the SDE.
