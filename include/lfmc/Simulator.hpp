b98bfb4efaadd49e047a8c7e621e7530514f20c6
Oliver Deng <oliver.t.deng@gmail.com>
2026-02-02 22:35:46 -0500

    refactor: refactor some of xander's code
    Absolutely no offense meant lol

--- include/lfmc/Simulator.hpp
+++ include/lfmc/Simulator.hpp
@@ -1,11 +1,11 @@
 #pragma once
+
 #include "NumericalScheme.hpp"
 #include "Payoff.hpp"
 #include "RandomGenerator.hpp"
 #include "StochasticProcess.hpp"
-#include "VarianceReductionStrategy.hpp"
+// #include "VarianceReductionStrategy.hpp"
 
-#include <numeric>
 // #include <thread>
 #include <vector>
 
@@ -19,62 +19,36 @@ template <StochasticProcess P, NumericalScheme<P> S, Payoff PO> class Simulator 
   public:
     // NOTE VRS is const pointer since we do not need ownership - alternatively use shared_ptr but
     // probably overkill? More importantly, will threads outlive the manager that owns the strategy?
-    Simulator(P process, S scheme, PO payoff, double x0, double T, size_t n_steps,
-              size_t n_simulations)
+    Simulator(P process, S scheme, PO payoff, double initialValue, double timeToMaturity,
+              size_t stepCount)
         : process_(std::move(process)), scheme_(std::move(scheme)), payoff_(std::move(payoff)),
-          x0_(x0), T_(T), n_steps_(n_steps), n_simulations_(n_simulations) {}
-    // TODO initialize other members
+          initialValue_(initialValue), timeToMaturity_(timeToMaturity), stepCount_(stepCount),
+          rng_() {}
 
-    // Used Claude to develope this sim. didn't feel like putting in the work since it will be
-    // replaced later...
-    double run_simulations() {
-        double sum = 0.0;
+    std::vector<double> generatePath() {
+        double dt = timeToMaturity_ / static_cast<float>(stepCount_);
+        std::vector<double> path;
+        path.reserve(stepCount_ + 1);
+        path.push_back(initialValue_);
+        std::vector<double> randomNormals = rng_.generateNormals(stepCount_);
 
-        for (size_t i = 0; i < n_simulations_; ++i) {
-            // Generate random normals for this path
-            auto randoms = rng_.generate_normals(n_steps_);
-
-            // Simulate terminal value
-            double terminal = scheme_.simulate_terminal(x0_, T_, n_steps_, randoms);
-
-            // Evaluate payoff
-            double payoff_value = payoff_(terminal);
-
-            sum += payoff_value;
+        double x = initialValue_;
+        for (size_t i{}; i < stepCount_; ++i) {
+            x = scheme_.step(process_, x, dt, randomNormals[i]);
+            path.push_back(x);
         }
-
-        // Return average (Monte Carlo estimate)
-        return sum / n_simulations_;
+        return path;
     }
 
-    /**
-     * @brief Run simulations and return both mean and standard error.
-     * @return Pair of (mean, standard_error).
-     */
-    std::pair<double, double> run_simulations_with_error() {
-        std::vector<double> payoffs(n_simulations_);
+    double generateTerminal() {
+        double dt = timeToMaturity_ / static_cast<float>(stepCount_);
+        std::vector<double> randomNormals = rng_.generateNormals(stepCount_);
 
-        for (size_t i = 0; i < n_simulations_; ++i) {
-            auto randoms = rng_.generate_normals(n_steps_);
-            double terminal = scheme_.simulate_terminal(x0_, T_, n_steps_, randoms);
-            payoffs[i] = payoff_(terminal);
+        double x = initialValue_;
+        for (size_t i{}; i < stepCount_; ++i) {
+            x = scheme_.step(process_, x, dt, randomNormals[i]);
         }
-
-        // Calculate mean
-        double mean = std::accumulate(payoffs.begin(), payoffs.end(), 0.0) / n_simulations_;
-
-        // Calculate standard deviation
-        double variance = 0.0;
-        for (double payoff : payoffs) {
-            double diff = payoff - mean;
-            variance += diff * diff;
-        }
-        variance /= (n_simulations_ - 1);
-
-        // Standard error = std_dev / sqrt(n)
-        double std_error = std::sqrt(variance / n_simulations_);
-
-        return {mean, std_error};
+        return x;
     }
 
     // TODO change return type?
@@ -91,11 +65,10 @@ template <StochasticProcess P, NumericalScheme<P> S, Payoff PO> class Simulator 
     P process_;
     S scheme_;
     PO payoff_;
-    double x0_;            // Initial value
-    double T_;             // Time to maturity
-    size_t n_steps_;       // Number of time steps
-    size_t n_simulations_; // Number of Monte Carlo paths
     RandomGenerator rng_;
+    double initialValue_;   // Initial value
+    double timeToMaturity_; // Time to maturity
+    size_t stepCount_;      // Number of time steps
 };
 
 } // namespace lfmc
