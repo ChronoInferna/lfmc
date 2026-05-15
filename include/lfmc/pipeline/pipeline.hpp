0a643ea3603a68d88e6f42de4345977de98bc2cc
Oliver Deng <oliver.t.deng@gmail.com>
2026-05-14 21:23:48 -0400

    fix: small fix

--- include/lfmc/pipeline/pipeline.hpp
+++ include/lfmc/pipeline/pipeline.hpp
@@ -32,8 +32,11 @@ template <StochasticProcess SP, NumericalScheme<SP> NS> class Pipeline {
                         return path_generator_->generate_paths(normals, steps, T);
                     })
                     .and_then([&](auto paths) { return payoff_->generate_payoffs(paths); })
-                    .and_then([&](auto payoffs) { return estimator_->add_payoffs(payoffs); })
-                    .or_else([](auto error) { return std::unexpected(error); });
+                    .and_then([&](auto payoffs) { return estimator_->add_payoffs(payoffs); });
+
+            if (!result) {
+                return std::unexpected(result.error());
+            }
         }
 
         return estimator_->result();
